a8177ca169785c0d9449c094db777a538d13c65d
ShovalShabi <shovalshabi@gmail.com>
2022-04-27 22:26:39 +0300

    Update Question7.py

--- Generators, Lambda Expressions and List comprehensions/Question7.py
+++ Generators, Lambda Expressions and List comprehensions/Question7.py
@@ -1,4 +1,3 @@
-#Presentor:Shoval Shabi ID:208383885
 ##################################################Time Generator Function###############################################
 def tick_clock(lst, jump):  #helper method for function
     lst[2]=lst[2] + jump[2]
