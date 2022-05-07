77cef22402034389b3f49949e72448cdc608f409
ShovalShabi <shovalshabi@gmail.com>
2022-05-07 12:23:16 +0300

    Update Question3.py

--- Decorator and observer design patterns/Question3.py
+++ Decorator and observer design patterns/Question3.py
@@ -1,5 +1,5 @@
 import math
-#Presentor:Shoval Shabi ID:208383885
+
 #one line lambda expression that returns natural root of a number if it's an integer else returns 0
 find_root=lambda x: int(math.sqrt(x)) if math.sqrt(x) %1==0 else 0
 
